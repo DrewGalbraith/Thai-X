2f0653bbeeed970dd45d939cb4817496527efca6
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-03-20 23:31:54 -0600

    Add imports

---
+++ nltk_parse.py
@@ -0,0 +1,4 @@
+from nltk.parse import recursivedescent as rdp  # https://www.nltk.org/_modules/nltk/parse/recursivedescent.html
+from english_grammar import grammar
+from nltk import CFG, ChartParser, RecursiveDescentParser, ShiftReduceParser, BottomUpChartParser, BottomUpLeftCornerChartParser, LeftCornerChartParser, LeftCornerChartParser
+
