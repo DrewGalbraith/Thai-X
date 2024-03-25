d4a4cab6a77fbf65151d3accd5a7788a2e89a7a1
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-03-24 22:47:09 -0600

    Move dynamic grammar generation function to a different file

--- nltk_parse.py
+++ nltk_parse.py
@@ -1,4 +1,16 @@
-from nltk.parse import recursivedescent as rdp  # https://www.nltk.org/_modules/nltk/parse/recursivedescent.html
-from english_grammar import grammar
+from nltk.parse import recursivedescent as rdp
 from nltk import CFG, ChartParser, RecursiveDescentParser, ShiftReduceParser, BottomUpChartParser, BottomUpLeftCornerChartParser, LeftCornerChartParser, LeftCornerChartParser
+from nltk.app import rdparser
+# https://github.com/emilmont/pyStatParser
 
+# rdparser()  # un/comment to toggle visualization tool
+
+# with open("english_grammar.txt", "r") as infile:
+#     eng_cfg = infile.read()
+
+# grammar = CFG.fromstring(eng_cfg)
+# parser = RecursiveDescentParser(grammar)
+# sent = 'the cat chased the dog'.split()
+# for tree in parser.parse(sent):
+#     print(tree)
+# Input Thai sentence
