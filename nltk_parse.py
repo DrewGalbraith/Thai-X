dbbec486a2914a052749d44faa072a3e47b766f6
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-04-16 22:19:28 -0600

    Create demo for Thai Recursive Descent Parser

--- nltk_parse.py
+++ nltk_parse.py
@@ -1,16 +1,21 @@
-from nltk.parse import recursivedescent as rdp
+import test_sentences
+
+from dynamic_grammar import dynamic_grammar_generator
 from nltk import CFG, ChartParser, RecursiveDescentParser, ShiftReduceParser, BottomUpChartParser, BottomUpLeftCornerChartParser, LeftCornerChartParser, LeftCornerChartParser
 from nltk.app import rdparser
-# https://github.com/emilmont/pyStatParser
+from nltk.app.rdparser_app import RecursiveDescentApp as RDA
+from nltk.parse import recursivedescent as rdp
 
-# rdparser()  # un/comment to toggle visualization tool
 
-# with open("english_grammar.txt", "r") as infile:
-#     eng_cfg = infile.read()
+# rdparser()  # uncomment to run the recursive descent parser demo
 
-# grammar = CFG.fromstring(eng_cfg)
-# parser = RecursiveDescentParser(grammar)
-# sent = 'the cat chased the dog'.split()
-# for tree in parser.parse(sent):
-#     print(tree)
-# Input Thai sentence
+with open("thai_grammar.txt", "r") as infile:
+    thai_phrase_struct_rules = infile.read()
+print(thai_phrase_struct_rules)
+
+thai_cfg = CFG.fromstring(thai_phrase_struct_rules)
+
+sentence = input("\n\nEnter your Thai sentence here: ")
+new_cfg = dynamic_grammar_generator(sentence, thai_cfg, lang='th')
+
+RDA(grammar=new_cfg, sent=sentence).mainloop()  # un/comment to toggle visualization tool
