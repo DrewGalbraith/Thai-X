ac3d98ab103bc5c611ce7543f6d77273c059e78d
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-04-16 22:23:54 -0600

    Create sentences for testing

---
+++ test_sentences.py
@@ -0,0 +1,15 @@
+eng_strings = ["Simone went to the park",  # ye ole sentence
+"It was Abdul that went to the park",  # non-complimentizer 'that'
+"Yusef had hard days aplenty ",  # adj (specifier? before and after noun)
+"Yulin said that Eros was losing his mind",  # explicit complimentizer 'that'
+"Eros said Yulin was losing her mind",  # null complimentizer 'that'
+"We want to sink the boat by the dock"]  # ambiguous
+
+eng_truth = []  # TODO: Evaluate English sentences above; 
+# TODO: ambiguous sentences should have multiple possible parses in a tuple
+
+th_strings = ["พี่น้องชาวบ้านกำลังเลี้ยงสตางค์ในสวน"]  # TODO: add more Thai sentences; 
+# TODO: at least one ambiguous and one null
+
+th_truth = []# TODO: Evaluate Thai sentences above; 
+# TODO: ambiguous sentences should have multiple possible parses in a tuple
